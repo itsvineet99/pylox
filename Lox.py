cc6dc6937faee01ce21aa5cb36e76e5667f9873c
Vineet Dorikar <vineetdorikar06@gmail.com>
2026-02-22 23:53:17 +0530

    implemented cli for interpret, file input and REPL

---
+++ Lox.py
@@ -0,0 +1,74 @@
+import sys
+from pathlib import Path
+
+from Scanner import Scanner
+
+had_err = False
+
+# interprete from file
+def run_file(path):
+    global had_err # letting know the function that we are using global variable
+
+    file_path = Path(path)
+
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            source = file.read()
+        run(source)
+
+        if had_err:
+            sys.exit(65)
+        
+    except FileNotFoundError: # Catching the specific error
+        print(f"Error: Could not find the file '{path}'.")
+        
+    except IOError: # A broader catch-all for permission issues, etc.
+        print(f"Error: Could not read the file '{path}'.")
+
+# REPL (Read-Eval-Print Loop), in short reading from terminal directly
+def run_prompt():
+    global had_err
+    
+    while True:
+        try:
+            line = input("> ")
+            run(line)
+            had_err = False
+        except EOFError:
+            # This triggers when the user presses Ctrl+D (Mac/Linux) or Ctrl+Z (Windows)
+            print() 
+            break
+
+
+def run(source):
+    scanner = Scanner(source)
+    tokens = scanner.scan_tokens()
+
+    for token in tokens:
+        print(token)
+
+# some error functions 
+def error(line_no, message):
+    report(line_no, "", message)
+
+def report(line_no, where, message):
+    global had_err 
+
+    # using file=sys.stderr redirects our output to err stream rather than general output stream
+    print(f"[line {line_no}] error{where}: {message}", file=sys.stderr)
+    had_err = True
+
+def main():
+    args = sys.argv[1:] # argv[0] is script name so we ignore it
+
+    if len(args) > 1:
+        print(f"Usage: pylox [script]")
+        sys.exit(64)
+    elif len(args) == 1:
+        run_file(args[0])
+    else:
+        # REPL
+        run_prompt()
+
+if __name__ == "__main__":
+    main()
