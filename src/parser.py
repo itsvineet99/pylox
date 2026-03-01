c651e49ca2ff4a829f954f920ed8280a2ddd3924
Vineet Dorikar <vineetdorikar06@gmail.com>
2026-03-01 18:09:05 +0530

    implemented for loop using desugaring(only making changes in front-end).

--- src/parser.py
+++ src/parser.py
@@ -178,6 +178,8 @@ class Parser:
             self.advance()
 
     def statement(self):
+        if self.match(TokenType.FOR):
+            return self.for_statement()
         if self.match(TokenType.IF):
             return self.if_statement()
         if self.match(TokenType.PRINT):
@@ -188,6 +190,46 @@ class Parser:
             return Block(self.block())
         else:
             return self.expression_statement()
+        
+    def for_statement(self):
+        self.consume(TokenType.LEFT_PAREN, "Expect '(' after 'for'.")
+
+        if self.match(TokenType.SEMICOLON):
+            initializer = None
+        elif self.match(TokenType.VAR):
+            initializer = self.var_declaration()
+        else:
+            initializer = self.expression_statement()
+
+        condition = None
+        if not self.check(TokenType.SEMICOLON):
+            condition = self.expression()
+        self.consume(TokenType.SEMICOLON, "Expect ';' after loop condition.")
+
+        increment = None
+        if not self.check(TokenType.RIGHT_PAREN):
+            increment = self.expression()
+        self.consume(TokenType.RIGHT_PAREN, "Expect ')' after for clauses.")
+
+        body = self.statement()
+
+        if increment is not None:
+            body = Block(
+                [body, Expression(increment)]
+            )
+        
+        if condition is None:
+            condition = Literal(True)
+        body = While(condition, body)
+
+        if initializer is not None:
+            body = Block(
+                [initializer, body]
+            )
+
+
+
+        return body
 
     def print_statement(self):
         value = self.expression()
